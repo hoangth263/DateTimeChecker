6c799aca2bf6817ee22456b900290cbee72b2391
hoangth263 <113684488+hoangth263@users.noreply.github.com>
2022-10-22 12:53:35 +0700

    Add files via upload

---
+++ test/CheckingControllerTest.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ACER
+ */
+public class CheckingControllerTest {
+    
+    public CheckingControllerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of processRequest method, of class CheckingController.
+     */
+    @Test
+    public void testProcessRequest() throws Exception {
+        System.out.println("processRequest");
+        HttpServletRequest request = null;
+        HttpServletResponse response = null;
+        CheckingController instance = new CheckingController();
+        instance.processRequest(request, response);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of form_hanler method, of class CheckingController.
+     */
+    @Test
+    public void testForm_hanler() throws Exception {
+        System.out.println("form_hanler");
+        HttpServletRequest request = null;
+        HttpServletResponse response = null;
+        CheckingController instance = new CheckingController();
+        instance.form_hanler(request, response);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of doGet method, of class CheckingController.
+     */
+    @Test
+    public void testDoGet() throws Exception {
+        System.out.println("doGet");
+        HttpServletRequest request = null;
+        HttpServletResponse response = null;
+        CheckingController instance = new CheckingController();
+        instance.doGet(request, response);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of doPost method, of class CheckingController.
+     */
+    @Test
+    public void testDoPost() throws Exception {
+        System.out.println("doPost");
+        HttpServletRequest request = null;
+        HttpServletResponse response = null;
+        CheckingController instance = new CheckingController();
+        instance.doPost(request, response);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getServletInfo method, of class CheckingController.
+     */
+    @Test
+    public void testGetServletInfo() {
+        System.out.println("getServletInfo");
+        CheckingController instance = new CheckingController();
+        String expResult = "";
+        String result = instance.getServletInfo();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
---
+++ test/DateTimeCheckerTest.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.ArrayList;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author HUNG
+ */
+public class DateTimeCheckerTest {
+
+    private class TestCase {
+
+        private byte day;
+        private byte month;
+        private short year;
+        private byte expResult;
+        private boolean expBooleanResult;
+
+        public TestCase(byte month, short year, byte expResult) {
+            this.month = month;
+            this.year = year;
+            this.expResult = expResult;
+        }
+
+        public TestCase(byte day, byte month, boolean expBooleanResult) {
+            this.day = day;
+            this.month = month;
+            this.expBooleanResult = expBooleanResult;
+        }
+
+        public byte getExpResult() {
+            return expResult;
+        }
+
+        public void setExpResult(byte expResult) {
+            this.expResult = expResult;
+        }
+
+        public boolean isExpBooleanResult() {
+            return expBooleanResult;
+        }
+
+        public void setExpBooleanResult(boolean expBooleanResult) {
+            this.expBooleanResult = expBooleanResult;
+        }
+
+        public TestCase() {
+        }
+
+        public byte getDay() {
+            return day;
+        }
+
+        public void setDay(byte day) {
+            this.day = day;
+        }
+
+        public byte getMonth() {
+            return month;
+        }
+
+        public void setMonth(byte month) {
+            this.month = month;
+        }
+
+        public short getYear() {
+            return year;
+        }
+
+        public void setYear(short year) {
+            this.year = year;
+        }
+
+    }
+
+    /**
+     * Test of checkDayInMonth method, of class DateTimeChecker.
+     */
+    @Test
+    public void testCheckDayInMonth() {
+        System.out.println("checkDayInMonth");
+        ArrayList<TestCase> allcase = new ArrayList<>();
+        allcase.add(new TestCase(Byte.parseByte("1"), Short.parseShort("2022"), Byte.parseByte("31")));
+        allcase.add(new TestCase(Byte.parseByte("3"), Short.parseShort("2022"), Byte.parseByte("31")));
+        allcase.add(new TestCase(Byte.parseByte("4"), Short.parseShort("2022"), Byte.parseByte("30")));
+        allcase.add(new TestCase(Byte.parseByte("5"), Short.parseShort("2022"), Byte.parseByte("31")));
+        allcase.add(new TestCase(Byte.parseByte("6"), Short.parseShort("2022"), Byte.parseByte("30")));
+        allcase.add(new TestCase(Byte.parseByte("7"), Short.parseShort("2022"), Byte.parseByte("31")));
+        allcase.add(new TestCase(Byte.parseByte("8"), Short.parseShort("2022"), Byte.parseByte("31")));
+        allcase.add(new TestCase(Byte.parseByte("9"), Short.parseShort("2022"), Byte.parseByte("30")));
+        allcase.add(new TestCase(Byte.parseByte("10"), Short.parseShort("2022"), Byte.parseByte("31")));
+        allcase.add(new TestCase(Byte.parseByte("11"), Short.parseShort("2022"), Byte.parseByte("30")));
+        allcase.add(new TestCase(Byte.parseByte("12"), Short.parseShort("2022"), Byte.parseByte("31")));
+        allcase.add(new TestCase(Byte.parseByte("2"), Short.parseShort("2000"), Byte.parseByte("29")));
+        allcase.add(new TestCase(Byte.parseByte("2"), Short.parseShort("2001"), Byte.parseByte("28")));
+        allcase.add(new TestCase(Byte.parseByte("2"), Short.parseShort("2022"), Byte.parseByte("28")));
+        allcase.add(new TestCase(Byte.parseByte("2"), Short.parseShort("1900"), Byte.parseByte("28")));
+        for (TestCase testCase : allcase) {
+            assertEquals("Test case: {"+testCase.getMonth()+", "+testCase.getYear()+", "+testCase.getExpResult()+"} fail."
+                    , testCase.getExpResult(), DateTimeChecker.checkDayInMonth(testCase.getMonth(),testCase.getYear()));
+        }
+    }
+
+    /**
+     * Test of isValidDate method, of class DateTimeChecker.
+     */
+    @Test
+    public void testIsValidDate() {
+        System.out.println("isValidDate");
+        
+
+    }
+
+}
